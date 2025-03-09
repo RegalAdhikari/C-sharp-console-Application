f2da1b9966e3f3dd6244a812ebed2c6f925cbee4
Aakriti Banjara <banjarabinda@gmail.com>
2025-03-09 17:31:55 +0545

    initial changes

---
+++ SecondApp/SecondApp/ChapterOne.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld;
+
+public class ChapterOne
+{
+    public static void ChapOne(string[] args)
+    {
+        // byte b = 1;
+        // int i = b;
+        // Console.WriteLine(i);
+        // int i = 1000;
+        // byte b = (byte)i;
+        // Console.WriteLine(b);
+        var number = "1234";
+        int i = int.Parse(number);
+        Console.WriteLine(i);
+    }
+}
---
+++ SecondApp/SecondApp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.50f;
+            char character = 'A';
+            string firstName = "Aakriti and Regal";
+            var isWorking = false;
+
+            Console.WriteLine(totalPrice + count);
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(character);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+
+
+            // Type conversion 
+            // int i = 1;
+            // byte b = i;  explicit 
+            // byte b = (byte)i;
+            string s = "1";
+            int i = Convert.ToInt32(s);
+            int j = int.Parse(s);
+            Console.WriteLine(s);
+            Console.WriteLine(j);
+            Console.WriteLine(i);
+
+
+            ChapterOne.ChapOne(args);
+        }
+        
+    }
+}
+
+// Console.WriteLine("Hello, World!");
